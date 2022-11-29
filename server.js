a56efedd0043c5bcfc5058339d9455565cae93f9
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2022-11-29 18:03:46 -0300

    fix

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const fs = require("fs");
+const app = express();
+app.use(express.json());
 
 let dados = []
+
+function escreveDados (){
+    fs.writeFileSync('./db.json', JSON.stringify(dados))
+}
+
 function getDados(){
     dados =  JSON.parse(fs.readFileSync('./db.json'))
     return dados;
 }
 
 
-const app = express();
+
 
 app.get("/users", function (req, res){
     const dados = getDados();
@@ -24,6 +31,26 @@ app.get("/users/:id", function (req, res){
     res.send(usuario);
     
  })
+
+app.post('/', function(req, res){
+    console.log(req.body)
+    res.send("Recebaaaaaaaaa");
+
+})
+
+app.post('/users', function(req, res){
+    const novoUsuario = {
+        nome: req.body.nome,
+        idade: req.body.idade,
+        id: req.body.id 
+    }
+    getDados()
+    dados.push(novoUsuario);
+    escreveDados();
+    res.sendStatus(201);
+    res.send("passou aqui")
+    console.log(dados)
+})
 
 app.listen(3000, () => console.log("Rodou"));
 
